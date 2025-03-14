cbfb05a268fefad9fe61baf6a5a3b23f8f0d0926
Ramjee upadhyay <4ramjee841436@gmail.com>
2025-03-14 20:27:42 +0530

    Delete src/Sorry_babu.java

--- src/Sorry_babu.java
+++
@@ -1,9 +0,0 @@
-public class Sorry_babu {
-    public static void main(String[] args) {
-        int x;
-        for(x=0;x<=100;x++)
-        {
-            System.out.println("Sorry life line");
-        }
-    }
-}
