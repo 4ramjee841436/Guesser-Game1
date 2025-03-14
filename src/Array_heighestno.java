5413e9428e15fa84ce45d192439aac1b9d98cab5
Ramjee upadhyay <4ramjee841436@gmail.com>
2025-03-14 20:17:07 +0530

    Delete src/Array_heighestno.java

--- src/Array_heighestno.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-// to input an array and find the highest number
-public class Array_heighestno {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-
-    }
-}
