46150bad362df09ced539c4c80d57012f80140a9
Ramjee upadhyay <4ramjee841436@gmail.com>
2025-03-14 20:16:37 +0530

    Delete src/practice_program/java directory

--- src/practice_program/java/Practice_set_03.java
+++
@@ -1,4 +0,0 @@
-package practice_program.java;
-
-public class Practice_set_03 {
-}
--- src/practice_program/java/Reverse.java
+++
@@ -1,9 +0,0 @@
-package practice_program.java;
-
-public class Reverse {
-    public static void main(String[] args) {
-        StringBuffer sb= new StringBuffer("Ramjee upadhyay");
-        sb.reverse();
-        System.out.println(sb);
-    }
-}
